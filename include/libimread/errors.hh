ce5c06e9398e3bc72a18139c808bbfb8a0709bed
Alexander Böhn <fish2000@gmail.com>
2015-08-03 19:37:43 -0400

    Whitespace

--- include/libimread/errors.hh
+++ include/libimread/errors.hh
@@ -32,11 +32,11 @@ namespace im {
     using disable_if_t = std::enable_if_t<!B, T>;
     
     /// apply_impl() and apply() courtesy of http://stackoverflow.com/a/19060157/298171
-    template<typename F, typename Tuple, std::size_t ...I> inline
+    template <typename F, typename Tuple, std::size_t ...I> inline
     auto apply_impl(F&& f, Tuple&& t, std::index_sequence<I...>) {
         return std::forward<F>(f)(std::get<I>(std::forward<Tuple>(t))...);
     }
-    template<typename F, typename Tuple> inline
+    template <typename F, typename Tuple> inline
     auto apply(F&& f, Tuple&& t) {
         using Indices = std::make_index_sequence<std::tuple_size<std::decay_t<Tuple>>::value>;
         return apply_impl(std::forward<F>(f), std::forward<Tuple>(t), Indices());
@@ -79,7 +79,7 @@ namespace im {
         /// adapted from http://stackoverflow.com/a/21806609/298171
         std::string out;
         int unpack[] __attribute__((unused)) { 0, 
-            (out += "\t" + im::stringify<Args>(args) + "\n", 0)...
+            (out += "\t" + im::stringify<Args>(args) + std::endl, 0)...
         };
         return out;
     }
