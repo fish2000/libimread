c81eec590e8a8f68423a1a5130efb9dcc6f73db0
Alexander Böhn <fish2000@gmail.com>
2015-08-14 06:19:46 -0400

    Re-enabled original objc::msg::send() test

--- tests/test_objc_rt.mm
+++ tests/test_objc_rt.mm
@@ -114,43 +114,46 @@ namespace {
         [stringArg release];
     }
     
-    // BLEST_CASE("[objc-rt] Send a message via objc::msg::send()", "[objc-rt-msg-send]") {
-    //     im::fs::NamedTemporaryFile temporary;
-    //     NSData *datum;
-    //     NSURL *url;
-    //     NSString *urlpath;
-    //     std::string path;
-    //     //std::string prefix = "file://";
-    //     std::string prefix = "";
-    //     std::size_t nbytes = 20 * 1024; /// 20480
-    //     unsigned char randos[20480] = {0};
-    //
-    //     arc4random_buf(static_cast<void*>(randos), nbytes);
-    //     path = prefix + temporary.str();
-    //
-    //     @autoreleasepool {
-    //         datum = [[NSData alloc] initWithBytes:(const void *)&randos[0]
-    //                                        length:(NSInteger)nbytes];
-    //         urlpath = [[NSString alloc] initWithUTF8String:path.c_str()];
-    //         url = [[NSURL alloc] initWithString:urlpath];
-    //
-    //         // [datum writeToFile:urlpath atomically:YES];
-    //
-    //         // objc::msg::send((id)datum,
-    //         //     objc::selector("writeToFile:atomically:"),
-    //         //     urlpath, YES);
-    //
-    //         // objc_msgSend((id)datum,
-    //         //     @selector(writeToFile:atomically:),
-    //         //     urlpath, YES);
-    //
-    //         #if !__has_feature(objc_arc)
-    //             [datum release];
-    //             [urlpath release];
-    //             [url release];
-    //         #endif
-    //     };
-    //
-    // }
+    TEST_CASE("[objc-rt] Send a message via objc::msg::send()", "[objc-rt-msg-send]") {
+        im::fs::NamedTemporaryFile temporary;
+        NSData *datum;
+        NSURL *url;
+        NSString *urlpath;
+        std::string path;
+        //std::string prefix = "file://";
+        std::string prefix = "";
+        std::size_t nbytes = 20 * 1024; /// 20480
+        unsigned char randos[20480] = {0};
+
+        arc4random_buf(static_cast<void*>(randos), nbytes);
+        path = prefix + temporary.str();
+
+        @autoreleasepool {
+            datum = [[NSData alloc] initWithBytes:(const void *)&randos[0]
+                                           length:(NSInteger)nbytes];
+            urlpath = [[NSString alloc] initWithUTF8String:path.c_str()];
+            url = [[NSURL alloc] initWithString:urlpath];
+            
+            // [datum writeToFile:urlpath atomically:YES];
+            
+            [datum retain];
+            [urlpath retain];
+            
+            objc::msg::send((id)datum,
+                objc::selector("writeToFile:atomically:"),
+                urlpath, YES);
+            
+            // objc_msgSend((id)datum,
+            //     @selector(writeToFile:atomically:),
+            //     urlpath, YES);
+            
+            #if !__has_feature(objc_arc)
+                [datum release];
+                [urlpath release];
+                [url release];
+            #endif
+        };
+
+    }
     
 }
