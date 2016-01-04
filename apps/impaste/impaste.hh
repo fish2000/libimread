8d7bda35d70ae2c6a34c2cac2f80b69dbd28c569
Alexander Böhn <fish2000@gmail.com>
2016-01-03 20:49:44 -0500

    Skeleton impaste app works, yay!

--- apps/impaste/impaste.hh
+++ apps/impaste/impaste.hh
@@ -4,11 +4,88 @@
 #ifndef APPS_IMPASTE_HH_
 #define APPS_IMPASTE_HH_
 
+#include <type_traits>
+
+#ifdef __APPLE__
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+#endif
+
 #include <libimread/libimread.hpp>
+
+/// printffery
+#define IMPASTE_DEBUG 0
+
+/// NSThread declarations and definitions,
+/// one(ish) for each CLI option
+
+@interface AXCheckThread : NSThread {}
+@end
+
+@interface AXDryRunThread : NSThread {}
+@end
+
+@interface AXImageSaveThread : NSThread {}
+@end
+
+/// boilerplate reduction strategem one:
+/// macro + naming convention
+
+#define AXTHREADRUN(threadName)                                                 \
+    @autoreleasepool {                                                          \
+        [NSApplication sharedApplication];                                      \
+        [[[AX##threadName##Thread alloc] init] start];                          \
+        [NSApp run];                                                            \
+    };
+
+
+namespace objc {
+    
+    /// boilerplate reduction strategem two:
+    /// inlined function template
+    
+    template <typename OCThreadType> inline
+    void run_thread(void) {
+        @autoreleasepool {
+            [NSApplication sharedApplication];
+            [[[OCThreadType alloc] init] start];
+            [NSApp run];
+        };
+    }
+    
+    namespace appkit {
+        
+        template <typename OCType>
+        BOOL can_paste(NSPasteboard* board = nil) noexcept {
+            if (!board) { board = [NSPasteboard generalPasteboard]; }
+            return [board canReadObjectForClasses:@[ [OCType class] ]
+                                          options:@{}];
+        }
+        
+        template <typename OCType>
+        __attribute__((ns_returns_retained))
+        typename std::add_pointer_t<OCType> paste(NSPasteboard* board = nil) noexcept {
+            if (!board) { board = [NSPasteboard generalPasteboard]; }
+            if (!can_paste<OCType>(board)) { return nil; }
+            NSArray* out = [board readObjectsForClasses:@[ [OCType class] ]
+                                                options:@{}];
+            
+            /// array is nil on error -- but empty if the call to
+            /// `readObjectsForClasses:options:` comes up short… SOOO:
+            return out == nil ? nil : out[0];
+        }
+        
+        
+    }
+    
+}
+
 
 namespace im {
     
     namespace config {
+        
+        const std::string version = IM_VERSION;
         
     };
     
--- apps/impaste/impaste.mm
+++ apps/impaste/impaste.mm
@@ -1,46 +1,43 @@
 
 #include <map>
+#include <atomic>
 #include <string>
 #include <iostream>
 
 #include "impaste.hh"
 #include "docopt.h"
 
+/// return value as static global (ugh I know I know)
+static std::atomic<int> return_value(EXIT_SUCCESS);
+
 /// NSThread declarations and definitions,
 /// one(ish) for each CLI option
 
-@protocol AXCopyReceiver <NSObject>
-- (NSArray<NSString*>*) 
-@end
-
-NSImage* getit() {
-    NSPasteboard* board = [NSPasteboard generalPasteboard];
-    // NSArray* classes = @[ [NSImage class] ];
-    // NSDictionary* options = @{};
-    
-    BOOL ok = [board canReadObjectsForClasses:@[ [NSImage class] ]
-                                      options:@{}];
-    if (!ok) { return nil; }
-    
-    // NSImage* image = [board readObjectsForClasses:@[ [NSImage class] ]
-    //                                       options:@{}][0];
-    // return image;
-    return [board readObjectsForClasses:@[ [NSImage class] ]
-                                options:@{}][0];
-}
-
-
-@interface AXCheckThread : NSThread {}
-@end
-
-@interface AXDryRunThread : NSThread {}
-@end
-
-@interface AXImageSaveThread : NSThread {}
-@end
-
 @implementation AXCheckThread : NSThread
 - (void) main {
+    
+    std::cout << "Checking default NSPasteboard for images ..."
+              << std::endl;
+    
+    BOOL ok = objc::appkit::can_paste<NSImage>();
+    
+    if (ok) {
+        
+        std::cout << "Pasteboard contains useable image data [go nuts!]"
+                  << std::endl;
+        return_value.store(EXIT_SUCCESS);
+        
+    } else { /// THIS IS NOT TOWARD
+        
+        std::cout << "No useable image data found [sorry dogg]"
+                  << std::endl;
+        return_value.store(EXIT_FAILURE);
+        
+    }
+    
+    /// exit from thread
+    [NSApp terminate:self];
+    return;
     
 }
 @end
@@ -48,11 +45,27 @@ NSImage* getit() {
 @implementation AXDryRunThread : NSThread
 - (void) main {
     
+    std::cout << "Dry run not implemented yet, exiting"
+              << std::endl;
+    return_value.store(EXIT_FAILURE);
+    
+    /// exit from thread
+    [NSApp terminate:self];
+    return;
+    
 }
 @end
 
 @implementation AXImageSaveThread : NSThread
 - (void) main {
+    
+    std::cout << "Image save not implemented yet, exiting"
+              << std::endl;
+    return_value.store(EXIT_FAILURE);
+    
+    /// exit from thread
+    [NSApp terminate:self];
+    return;
     
 }
 @end
@@ -63,9 +76,11 @@ NSImage* getit() {
 static const char USAGE[] = R"(Paste image data to imgur.com or to a file
 
     Usage:
-        impaste       [options]
-        impaste       (-h | --help)
-        impaste        --version
+        impaste       (-c      | --check)
+        impaste       (-d      | --dry-run)
+        impaste       (-o FILE | --output=FILE)
+        impaste       (-h      | --help)
+        impaste                  --version
     
     Options:
         -c --check          Check and report on the pasteboard contents.
@@ -79,60 +94,58 @@ static const char USAGE[] = R"(Paste image data to imgur.com or to a file
 
 const std::string VERSION = "impaste ";
 
+
 int main(int argc, const char** argv) {
     using value_t = docopt::value;
     using optmap_t = std::map<std::string, value_t>;
     using optpair_t = std::pair<std::string, value_t>;
-    value_t truth(true);
+    value_t truth(1);
     optmap_t args;
     optmap_t raw_args = docopt::docopt(USAGE, { argv + 1, argv + argc },
                                        true, /// show help
                                        VERSION + im::config::version);
     
-    std::cout << "RAW ARGS:" << std::endl;
-    for (optpair_t const& arg : raw_args) {
-        std::cout << arg.first << " --> " << arg.second << std::endl;
-    }
-    std::cout << std::endl;
+    #if IMPASTE_DEBUG
+        std::cout << "RAW ARGS:" << std::endl;
+        for (optpair_t const& arg : raw_args) {
+            std::cout << arg.first << " --> " << arg.second << std::endl;
+        }
+        std::cout << std::endl;
+    #endif
     
     /// filter out all docopt parse artifacts,
     /// leaving only things beginning with "--"
     std::copy_if(raw_args.begin(), raw_args.end(),
                  std::inserter(args, args.begin()),
-                 [](const optpair_t& p) { return p.first.substr(0, 2) == "--"; });
+                 [](optpair_t const& p) { return p.first.substr(0, 2) == "--"; });
     
-    std::cout << "FILTERED ARGS:" << std::endl;
-    for (optpair_t const& arg : args) {
-        std::cout << arg.first << " --> " << arg.second << std::endl;
-    }
-    std::cout << std::endl;
+    #if IMPASTE_DEBUG
+        std::cout << "FILTERED ARGS:" << std::endl;
+        for (optpair_t const& arg : args) {
+            std::cout << arg.first << " --> " << arg.second << std::endl;
+        }
+        std::cout << std::endl;
+    #endif
     
     /// print the value for the truthy option flag
     for (optpair_t const& arg : args) {
         if (arg.second == truth) {
             if (arg.first == "--check") {
                 /* DO CHECK */
-                @autoreleasepool {
-                    [NSApplication sharedApplication];
-                    [NSApp run];
-                }
+                objc::run_thread<AXCheckThread>();
                 break;
             } else if (arg.first == "--dry-run") {
                 /* DO DRY RUN */
-                @autoreleasepool {
-                    [NSApplication sharedApplication];
-                    [NSApp run];
-                }
+                objc::run_thread<AXDryRunThread>();
                 break;
             } else if (arg.first == "--output") {
                 /* DO FILE OUTPUT */
-                @autoreleasepool {
-                    [NSApplication sharedApplication];
-                    [NSApp run];
-                }
+                objc::run_thread<AXImageSaveThread>();
                 break;
             }
         }
     }
+    
+    std::exit(return_value.load());
 
 }
