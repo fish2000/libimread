c79c6cebdd1b413ebc46206a669de05877a97d58
Alexander Böhn <fish2000@gmail.com>
2016-01-21 04:36:17 -0500

    Cleaned the Byzantine gunk out of the copy/paste tests

--- tests/test_appkit_copy_paste.mm
+++ tests/test_appkit_copy_paste.mm
@@ -22,10 +22,6 @@
 namespace {
     
     using filesystem::path;
-    using im::byte;
-    using im::Image;
-    // using im::InterleavedFactory;
-    using im::HalideFactory;
     
     TEST_CASE("[appkit-copy-paste] Copy and paste PNG image data",
               "[appkit-copy-paste-png-image-data]")
@@ -60,41 +56,10 @@ namespace {
                 NSURL* url = objc::appkit::paste<NSURL>(board);
                 NSImage* boardimage = objc::appkit::paste<NSImage>(board);
                 NSImage* mapimage = images.at([url filesystemPath]);
+                NSData* boarddata = [boardimage TIFFRepresentation];
+                NSData* mapdata = [mapimage TIFFRepresentation];
                 
-                // InterleavedFactory factory;
-                HalideFactory<byte> factory;
-                std::hash<Image> hasher;
-                std::unique_ptr<Image> boardim;
-                std::unique_ptr<Image> mapim;
-                NSBitmapImageRep* boardrep;
-                NSBitmapImageRep* maprep;
-                
-                for (id rep in boardimage.representations) {
-                    if ([rep isKindOfClass:[NSBitmapImageRep class]]) {
-                        boardrep = (NSBitmapImageRep*)rep;
-                        boardim = [boardrep imageUsingImageFactory:&factory];
-                        break;
-                    }
-                }
-                for (id rep in mapimage.representations) {
-                    if ([rep isKindOfClass:[NSBitmapImageRep class]]) {
-                        maprep = (NSBitmapImageRep*)rep;
-                        mapim = [maprep imageUsingImageFactory:&factory];
-                        break;
-                    }
-                }
-                
-                CHECK(boardim->size() > 0);
-                CHECK(mapim->size() > 0);
-                CHECK(boardim->size() == mapim->size());
-                
-                auto boardhash = blockhash::blockhash(*boardim).to_ulong();
-                auto maphash = blockhash::blockhash(*mapim).to_ulong();
-                
-                // CHECK(objc::to_bool([boardimage isEqual:mapimage]));
-                // CHECK(hasher(*boardim) == hasher(*mapim));
-                CHECK(boardhash == maphash);
-                
+                CHECK(objc::to_bool([boarddata isEqualToData:mapdata]));
             });
             
             std::for_each(boards.begin(), boards.end(), [&](NSPasteboard* board) {
@@ -139,45 +104,10 @@ namespace {
                 NSURL* url = objc::appkit::paste<NSURL>(board);
                 NSImage* boardimage = objc::appkit::paste<NSImage>(board);
                 NSImage* mapimage = images.at([url filesystemPath]);
-                
-                // InterleavedFactory factory;
-                // HalideFactory<byte> factory;
-                // std::hash<Image> hasher;
-                // std::unique_ptr<Image> boardim;
-                // std::unique_ptr<Image> mapim;
-                // NSBitmapImageRep* boardrep;
-                // NSBitmapImageRep* maprep;
                 NSData* boarddata = [boardimage TIFFRepresentation];
                 NSData* mapdata = [mapimage TIFFRepresentation];
                 
                 CHECK(objc::to_bool([boarddata isEqualToData:mapdata]));
-                
-                // for (id rep in boardimage.representations) {
-                //     if ([rep isKindOfClass:[NSBitmapImageRep class]]) {
-                //         boardrep = (NSBitmapImageRep*)rep;
-                //         boardim = [boardrep imageUsingImageFactory:&factory];
-                //         break;
-                //     }
-                // }
-                // for (id rep in mapimage.representations) {
-                //     if ([rep isKindOfClass:[NSBitmapImageRep class]]) {
-                //         maprep = (NSBitmapImageRep*)rep;
-                //         mapim = [maprep imageUsingImageFactory:&factory];
-                //         break;
-                //     }
-                // }
-                
-                // CHECK(boardim->size() > 0);
-                // CHECK(mapim->size() > 0);
-                // CHECK(boardim->size() == mapim->size());
-                //
-                // auto boardhash = blockhash::blockhash(*boardim).to_ulong();
-                // auto maphash = blockhash::blockhash(*mapim).to_ulong();
-                
-                // CHECK(objc::to_bool([boardimage isEqual:mapimage]));
-                // CHECK(hasher(*boardim) == hasher(*mapim));
-                // CHECK(boardhash == maphash);
-                
             });
             
             std::for_each(boards.begin(), boards.end(), [&](NSPasteboard* board) {
