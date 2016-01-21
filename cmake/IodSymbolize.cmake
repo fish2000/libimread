f34ad4d537dda36ad444e65041dadc43fe2ceab7
Alexander Böhn <fish2000@gmail.com>
2016-01-20 19:28:28 -0500

    The IOD_SYMBOLIZE() cmake macro: now with variadic arity GET YOURS TODAY

--- cmake/IodSymbolize.cmake
+++ cmake/IodSymbolize.cmake
@@ -1,19 +1,26 @@
 
-# macro(IOD_SYMBOLIZE_DIR directory)
-# endmacro()
-#
-# macro(ADD_IOD_SYMBOLS)
-# endmacro()
-
 include(FindPythonInterp)
 
-macro(IOD_SYMBOLIZE dir0 dir1 header)
-add_custom_command(
-    OUTPUT ${header}
-    COMMAND symbolizer --verbose ${dir0} ${dir1} -o ${header})
-add_custom_target("iod_symbolize"
-    DEPENDS ${header})
-set_source_files_properties(
-    ${header}
-    PROPERTIES GENERATED TRUE)
+# function(JOIN VALUES GLUE OUTPUT)
+#   string(REPLACE ";" "${GLUE}" _TMP_STR "${VALUES}")
+#   set(${OUTPUT} "${_TMP_STR}" PARENT_SCOPE)
+# endfunction()
+
+macro(IOD_SYMBOLIZE header)
+    set(symbolizer_args ${ARGN})
+    list(LENGTH symbolizer_args num_dir_args)
+    if(NOT ${num_dir_args} GREATER 0)
+        message(FATAL_ERROR
+            "IOD_SYMBOLIZE() called without directories")
+    endif()
+    list(INSERT symbolizer_args 0 symbolizer --verbose)
+    list(APPEND symbolizer_args -o ${header})
+    add_custom_command(
+        OUTPUT ${header}
+        COMMAND ${symbolizer_args})
+    add_custom_target("iod_symbolize"
+        DEPENDS ${header})
+    set_source_files_properties(
+        ${header}
+        PROPERTIES GENERATED TRUE)
 endmacro()
