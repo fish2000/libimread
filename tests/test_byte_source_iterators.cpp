3c938288d8fb8a8476b3b8dfcf6e99173dcb6e96
Alexander Böhn <fish2000@gmail.com>
2016-06-20 03:54:07 -0400

    Test for im::handle::source iterators

--- tests/test_byte_source_iterators.cpp
+++ tests/test_byte_source_iterators.cpp
@@ -11,6 +11,7 @@
 #include <libimread/seekable.hh>
 #include <libimread/image.hh>
 #include <libimread/file.hh>
+#include <libimread/filehandle.hh>
 
 #include "include/test_data.hpp"
 #include "include/catch.hpp"
@@ -19,10 +20,11 @@ namespace {
     
     using im::byte;
     using im::FileSource;
+    using HandleSource = im::handle::source;
     using filesystem::path;
     
-    TEST_CASE("[byte-source-iterators] Test byte_source iterators",
-              "[byte-source-iterators-test-byte_source-iterators]")
+    TEST_CASE("[byte-source-iterators] Test FileSource iterators",
+              "[byte-source-iterators-test-FileSource-iterators]")
     {
         path basedir(im::test::basedir);
         const std::vector<path> pngs = basedir.list("*.png");
@@ -43,10 +45,32 @@ namespace {
                              std::equal_to<byte>()));
             
         });
-        
-        
     }
     
+    
+    TEST_CASE("[byte-source-iterators] Test im::handle::source iterators",
+              "[byte-source-iterators-test-handle-source-iterators]")
+    {
+        path basedir(im::test::basedir);
+        const std::vector<path> pngs = basedir.list("*.png");
+        
+        std::for_each(pngs.begin(), pngs.end(), [&](path const& p) {
+            path imagepath = basedir/p;
+            std::string pth = imagepath.str();
+            HandleSource source(pth);
+            std::vector<byte> data;
+            std::for_each(source.begin(), source.end(), [&data](byte b) {
+                data.push_back(b);
+            });
+            std::vector<byte> fulldata(source.full_data());
+            
+            CHECK(data.size() == fulldata.size());
+            CHECK(std::equal(data.begin(),     data.end(),
+                             fulldata.begin(), fulldata.end(),
+                             std::equal_to<byte>()));
+            
+        });
+    }
     
 }
 
