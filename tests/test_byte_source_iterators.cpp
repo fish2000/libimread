1c740c1c8d91cd27e4b0afbd358a8c44ba379895
Alexander Böhn <fish2000@gmail.com>
2016-06-20 03:48:18 -0400

    HEY THERE WHO ALL OF YOU LIKES ITERABLE BYTE SOURCES

---
+++ tests/test_byte_source_iterators.cpp
@@ -0,0 +1,52 @@
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <algorithm>
+#include <unordered_map>
+
+#include <libimread/libimread.hpp>
+#include <libimread/ext/filesystem/path.h>
+#include <libimread/iterators.hh>
+#include <libimread/seekable.hh>
+#include <libimread/image.hh>
+#include <libimread/file.hh>
+
+#include "include/test_data.hpp"
+#include "include/catch.hpp"
+
+namespace {
+    
+    using im::byte;
+    using im::FileSource;
+    using filesystem::path;
+    
+    TEST_CASE("[byte-source-iterators] Test byte_source iterators",
+              "[byte-source-iterators-test-byte_source-iterators]")
+    {
+        path basedir(im::test::basedir);
+        const std::vector<path> pngs = basedir.list("*.png");
+        
+        std::for_each(pngs.begin(), pngs.end(), [&](path const& p) {
+            path imagepath = basedir/p;
+            std::string pth = imagepath.str();
+            FileSource source(pth);
+            std::vector<byte> data;
+            std::for_each(source.begin(), source.end(), [&data](byte b) {
+                data.push_back(b);
+            });
+            std::vector<byte> fulldata(source.full_data());
+            
+            CHECK(data.size() == fulldata.size());
+            CHECK(std::equal(data.begin(),     data.end(),
+                             fulldata.begin(), fulldata.end(),
+                             std::equal_to<byte>()));
+            
+        });
+        
+        
+    }
+    
+    
+}
+
