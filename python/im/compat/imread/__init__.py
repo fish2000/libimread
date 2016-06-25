14be5160b1c63ce1c84fb2cc56b0582a5f88ce20
Alexander Böhn <fish2000@gmail.com>
2016-06-25 04:05:22 -0400

    Wholesale lifting-in of imread API by @luispedro ... you do `from im.compat import imread` and it works thereafter ... we provide the Python top-level luispedro-authored `imread` module,     and a little-bitty `_imread` shim that uses our types and functions     to provide the necessary backing functionality ... we also include `imread.special`

---
+++ python/im/compat/imread/__init__.py
@@ -0,0 +1,15 @@
+
+from .imread import (
+    imread, imload,
+    imread_from_blob,
+    imload_from_blob,
+    imread_multi,
+    imload_multi,
+    
+    imsave,
+    imsave_multi,
+    imwrite,
+    imwrite_multi,
+    
+    detect_format,
+    supports_format)
