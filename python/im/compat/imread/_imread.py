1cc1dec9ae1327b921dc740390abe924633160e2
Alexander Böhn <fish2000@gmail.com>
2016-06-25 05:01:03 -0400

    Compatibility tests for @luispedro imread shim ... had to special-case 'jpeg' which imread uses as a format name     instead of 'jpg'

--- python/im/compat/imread/_imread.py
+++ python/im/compat/imread/_imread.py
@@ -24,7 +24,7 @@ def imread(filename, formatstr, flags):
     return (numpy.array(im.Array(filename)).transpose(1, 0, 2), '')
 
 def imread_from_blob(blob, formatstr, flags):
-    return numpy.array(im.Array(blob, as_blob=True)).transpose(1, 0, 2)
+    return (numpy.array(im.Array(blob, is_blob=True)).transpose(1, 0, 2), '')
 
 def imread_multi(filename, formatstr, flags):
     raise NotImplementedError("_imread.imread_multi is as-of-yet unsupported")
@@ -38,7 +38,12 @@ def imsave_multi(filename, formatstr, arrays, opts):
     raise NotImplementedError("_imread.imsave_multi is as-of-yet unsupported")
 
 def detect_format(filename_or_blob, is_blob):
-    return im.detect(filename_or_blob, bool(is_blob))
+    detected_format = im.detect(filename_or_blob, is_blob=bool(is_blob))
+    if detected_format == 'jpg':
+        return 'jpeg'
+    return detected_format
 
 def supports_format(formatstr):
+    if formatstr.lower() == 'jpeg':
+        formatstr = 'jpg'
     return formatstr.lower() in im.formats
--- python/im/tests/test_compat.py
+++ python/im/tests/test_compat.py
@@ -6,6 +6,39 @@ import im
 
 class CompatibilityTests(BaseCase):
     
+    def test_imread_imread(self):
+        import numpy
+        import imread as luispedro
+        from im.compat import imread
+        for image_path in self.jpgs:
+            ar0 = imread.imread(image_path)
+            ar1 = luispedro.imread(image_path)
+            self.assertTrue(numpy.all(ar0 == ar1))
+    
+    def test_imread_imread_from_blob(self):
+        import numpy
+        import imread as luispedro
+        from im.compat import imread
+        for image_path in self.jpgs:
+            with open(image_path, 'rb') as fh:
+                blob = fh.read()
+                ar0 = imread.imread_from_blob(blob)
+                ar1 = luispedro.imread_from_blob(blob)
+                self.assertTrue(numpy.all(ar0 == ar1))
+    
+    def test_imread_detect_format(self):
+        import imread as luispedro
+        from im.compat import imread
+        for image_path in self.jpgs:
+            format0 = imread.detect_format(image_path)
+            format1 = luispedro.detect_format(image_path)
+            self.assertEqual(format0, format1)
+            with open(image_path, 'rb') as fh:
+                blob = fh.read()
+                blobformat0 = imread.detect_format(blob, is_blob=True)
+                blobformat1 = luispedro.detect_format(blob, is_blob=True)
+                self.assertEqual(blobformat0, blobformat1)
+    
     def test_jpg_convert_to_PIL(self):
         ''' Load some JPG files,
             convert to PIL.Image instances,
@@ -41,7 +74,7 @@ class CompatibilityTests(BaseCase):
                 image = ImageType(image_path)
                 # self.assertIsNotNone(image)
                 pil_image = im.to_PIL(image, mode="RGB")
-                # self.assertIsNotNone(pil_image)
+                self.assertIsNotNone(pil_image)
                 ''' BUGGGG: TIFs are still fucking transposed '''
                 # self.assertEqual(pil_image.width,  image.width)
                 # self.assertEqual(pil_image.height, image.height)
