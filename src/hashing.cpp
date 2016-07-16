555d45f418bde9a3acca78842e7f6173fb02fd80
Alexander Böhn <fish2000@gmail.com>
2016-07-16 02:18:52 -0400

    WANTED: an array_view that isn't boosted (or CamelCased)

--- src/hashing.cpp
+++ src/hashing.cpp
@@ -302,8 +302,8 @@ namespace butteraugli {
             return error_images_incomprable;
         }
         
-        pixvec_t rgb0(auglize(lhs.allplanes<double>(2))); /// lastplane=0..2
-        pixvec_t rgb1(auglize(rhs.allplanes<double>(2))); /// lastplane=0..2
+        pixvec_t rgb0(auglize(lhs.allplanes<double>(3))); /// lastplane=0..2
+        pixvec_t rgb1(auglize(rhs.allplanes<double>(3))); /// lastplane=0..2
         
         if (rgb0.size() != 3 || rgb1.size() != 3) {
             return error_unexpected_channel_count;
@@ -329,8 +329,8 @@ namespace butteraugli {
             return error_images_incomprable;
         }
         
-        pixvec_t rgb0(auglize(lhs->allplanes<double>(2))); /// lastplane=0..2
-        pixvec_t rgb1(auglize(rhs->allplanes<double>(2))); /// lastplane=0..2
+        pixvec_t rgb0(auglize(lhs->allplanes<double>(3))); /// lastplane=0..2
+        pixvec_t rgb1(auglize(rhs->allplanes<double>(3))); /// lastplane=0..2
         
         if (rgb0.size() != 3 || rgb1.size() != 3) {
             return error_unexpected_channel_count;
