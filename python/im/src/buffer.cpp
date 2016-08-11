680aec44d8b57b99d36f9768cce9337e5bdefff7
Alexander Böhn <fish2000@gmail.com>
2016-08-11 15:33:10 -0400

    Notes and general cleanup in python/im/src/buffer.cpp ... my love affair with std::array is evidently still in full bloom

--- python/im/src/buffer.cpp
+++ python/im/src/buffer.cpp
@@ -1,5 +1,6 @@
 /// Copyright 2012-2015 Alexander Bohn <fish2000@gmail.com>
 /// License: MIT (see COPYING.MIT file)
+#include <array>
 #define NO_IMPORT_ARRAY
 #include <numpy/arrayobject.h>
 #include "buffer.hpp"
@@ -8,8 +9,11 @@ namespace im {
     
     namespace buffer {
         
+        /// COPYING HEAPCOPY: im::buffer::heapcopy(buffer_t buffer) ...
+        /// heap-allocating copy-construction analogue
+        
         buffer_t* heapcopy(buffer_t const* buffer) {
-            buffer_t* out = new buffer_t{
+            return new buffer_t{
                 buffer->dev,
                 buffer->host,
                 { buffer->extent[0], buffer->extent[1], buffer->extent[2], buffer->extent[3] },
@@ -19,36 +23,57 @@ namespace im {
                 buffer->host_dirty,
                 buffer->dev_dirty
             };
-            return out;
         }
         
+        /// SCALING HEAPCOPY: im::buffer::heapcopy(buffer_t buffer, float scale) ...
+        /// assumes planar arrangement, ignores mins (for now) --
+        /// NB. is it retarded to just copy over the dev/host pointers here?
+        /// ... like I'm just saying dogg I can't really concieve of a case
+        /// in which that would actually work without meddling
+        
         buffer_t* heapcopy(buffer_t const* buffer, float scale) {
-            /// assumes planar arrangement, ignores mins:
-            buffer_t* out = new buffer_t{
+            
+            std::array<int32_t, 4> extents{{ static_cast<int32_t>(buffer->extent[0] * scale),
+                                             static_cast<int32_t>(buffer->extent[1] * scale),
+                                             static_cast<int32_t>(buffer->extent[2] * scale),
+                                             static_cast<int32_t>(buffer->extent[3] * scale) }};
+            
+            std::array<int32_t, 4> stridings{{ 1, extents[0],
+                                                  extents[0]*extents[1],
+                                                  extents[0]*extents[1]*extents[2] }};
+            
+            return new buffer_t{
                 buffer->dev,
                 buffer->host,
-                {
-                    static_cast<int32_t>(buffer->extent[0] * scale),
-                    static_cast<int32_t>(buffer->extent[1] * scale),
-                    static_cast<int32_t>(buffer->extent[2] * scale),
-                    static_cast<int32_t>(buffer->extent[3] * scale)
-                },
-                {
-                    1,
-                    static_cast<int32_t>(buffer->extent[0] * scale),
-                    static_cast<int32_t>(buffer->extent[0] * buffer->extent[1] * scale),
-                    0
-                },
+                { extents[0],   extents[1],   extents[2],   extents[3] },
+                { stridings[0], stridings[1], stridings[2], stridings[3] },
                 { 0, 0, 0, 0 },
                 buffer->elem_size,
                 buffer->host_dirty,
-                buffer->dev_dirty
-            };
-            return out;
+                buffer->dev_dirty };
         }
         
+        /// CONVERTING HEAPCOPY: im::buffer::heapcopy(Py_buffer pybuffer) ...
+        /// heap-allocating conversion-construction analogue --
+        /// NB. in an ideal world -- a less-dirty world -- this would inspect
+        /// the pybuffer->format member, which maybe points to a c-string with
+        /// a structcode that we could use to, like, calculate and/or validate
+        /// some of this shit here
+        
+        /// ... actually now that I think of it, actually, code like that should
+        /// get abstracted (hear me out!) into a traited template thingy -- like this:
+        ///
+        ///     im::buffer::traits<buffer_t>::pixel_t               -> typename uint8_t
+        ///     im::buffer::traits<buffer_t>::value_t               -> typename int32_t (extent, etc)
+        ///     im::buffer::traits<buffer_t>::width(buffer)         -> whatevs
+        ///     im::buffer::traits<Py_buffer>::height(pybuffer)     -> IDK TBH
+        ///     im::buffer::traits<PyArrayObject>::data(array)      -> dogg it's just a data pointer
+        ///                                                            that is what the pixels are
+        ///
+        /// ... RIGHT!??! That would be cool, and not overly architecture-astronaut-y. Yes!
+        
         buffer_t* heapcopy(Py_buffer const* pybuffer) {
-            buffer_t* out = new buffer_t{
+            return new buffer_t{
                 0,
                 reinterpret_cast<uint8_t*>(pybuffer->buf),
                 { static_cast<int32_t>(pybuffer->shape[0]),
@@ -64,8 +89,10 @@ namespace im {
                 false,
                 false
             };
-            return out;
         }
+        
+        /// INSPECTING HEAPCOPY: im::buffer::heapcopy(PyArrayObject* array) ...
+        /// heap-allocating explicit-inspection-construction analogue
         
         buffer_t* heapcopy(PyArrayObject* array) {
             buffer_t* out;
@@ -150,6 +177,12 @@ namespace im {
             }
             return out;
         }
+        
+        /// HEAPCOPY DESTRUCTOR: im::buffer::heapdestroy(buffer_t buffer) ...
+        /// straightforward heap-allocation destructor analogue --
+        /// buffer creation is normalized to the point where there aren't any
+        /// variants necessitating per-member deallocation strategies
+        /// or suchlike (at least not yet -- fingers crossed doggie)
         
         void heapdestroy(buffer_t* buffer) {
             // delete buffer->extent;
