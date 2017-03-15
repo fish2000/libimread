e07a23bb37436d655c2069f3e6a9cab137c1bbda
Alexander Böhn <fish2000@gmail.com>
2017-03-15 02:39:26 -0400

    Return NULL_STR (in leu of throwing) from filesystem::detail::execute(…)

--- src/ext/filesystem/execute.cpp
+++ src/ext/filesystem/execute.cpp
@@ -1,8 +1,9 @@
 
 #include <array>
-#include <stdexcept>
+#include <memory>
 #include <fcntl.h>
 #include <unistd.h>
+#include <libimread/libimread.hpp>
 #include <libimread/ext/filesystem/execute.h>
 #include <libimread/ext/filesystem/path.h>
 #include <libimread/ext/filesystem/directory.h>
@@ -16,9 +17,7 @@ namespace filesystem {
             std::string result;
             std::unique_ptr<FILE, decltype(::pclose)&> pipe(::popen(command, "r"), ::pclose);
             switchdir s(workingdir ? path(workingdir) : path::tmp());
-            if (!pipe.get()) {
-                throw std::runtime_error("detail::execute(): ::popen() failed");
-            }
+            if (!pipe.get()) { return NULL_STR; }
             while (!std::feof(pipe.get())) {
                 if (std::fgets(buffer.data(), EXECUTION_BUFFER_SIZE, pipe.get()) != nullptr) {
                     result += buffer.data();
