5957ee869030f48aa0672132b2899760de2ecf8c
Alexander Böhn <fish2000@gmail.com>
2017-06-09 02:27:12 -0400

    Removed Cap‘n’P hooks in im::fd_source_sink

--- src/file.cpp
+++ src/file.cpp
@@ -7,7 +7,6 @@
 #include <sys/resource.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
-// #include <capnp/serialize-packed.h>
 
 #include <libimread/libimread.hpp>
 #include <libimread/errors.hh>
@@ -94,12 +93,6 @@ namespace im {
             static_cast<const void*>(&bv[0]),
             bv.size());
     }
-    
-    // std::size_t fd_source_sink::write(capnp::MessageBuilder& builder) {
-    //     capnp::writePackedMessageToFd(descriptor, builder);
-    //     // return capnp::computeUnpackedSizeInWords(builder.getSegmentsForOutput());
-    //     return capnp::computeSerializedSizeInWords(builder);
-    // }
     
     detail::stat_t fd_source_sink::stat() const {
         detail::stat_t info;
