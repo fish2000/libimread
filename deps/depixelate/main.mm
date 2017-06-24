99ac4b83acd0ebfb097ca46ef198e133a9f9d23f
Alexander Böhn <fish2000@gmail.com>
2017-06-24 08:56:10 -0400

    I am going full retard on the formatting on this one file for some reason

--- deps/depixelate/main.mm
+++ deps/depixelate/main.mm
@@ -24,53 +24,57 @@ using scale_f = std::add_pointer_t<bool(const void* srcData,
                                               int width, int height, int scale)>;
 
 typedef struct {
-    const char*   name;
-    const int     minScale;
-    const int     maxScale;
-    scale_f       scaler;
-} Algorithm;
+    const char* name;
+    const int   minScale;
+    const int   maxScale;
+    scale_f     scaler;
+} algorithm;
 
-static bool scale_xbrz(const void* srcData, void* dstData, int width, int height, int scale) {
-    xbrz::scale(scale, static_cast<uint32_t*>(const_cast<void*>(srcData)),
-                       static_cast<uint32_t*>(dstData),
-                       width, height);
-    return true;
-}
-
-static bool scale_hqx(const void* srcData, void* dstData, int width, int height, int scale) {
-    hqxInit();
-    switch (scale) {
-        case 2: hq2x_32(static_cast<uint32_t*>(const_cast<void*>(srcData)),
-                        static_cast<uint32_t*>(dstData),
-                        width, height);
-        case 3: hq3x_32(static_cast<uint32_t*>(const_cast<void*>(srcData)),
-                        static_cast<uint32_t*>(dstData),
-                        width, height);
-        case 4: hq4x_32(static_cast<uint32_t*>(const_cast<void*>(srcData)),
-                        static_cast<uint32_t*>(dstData),
-                        width, height);
+namespace scale {
+    
+    static bool xbrz(const void* srcData, void* dstData, int width, int height, int scale) {
+        xbrz::scale(scale, static_cast<uint32_t*>(const_cast<void*>(srcData)),
+                           static_cast<uint32_t*>(dstData),
+                           width, height);
+        return true;
     }
-    return true;
-}
+    
+    static bool hqx(const void* srcData, void* dstData, int width, int height, int scale) {
+        hqxInit();
+        switch (scale) {
+            case 2: hq2x_32(static_cast<uint32_t*>(const_cast<void*>(srcData)),
+                            static_cast<uint32_t*>(dstData),
+                            width, height);
+            case 3: hq3x_32(static_cast<uint32_t*>(const_cast<void*>(srcData)),
+                            static_cast<uint32_t*>(dstData),
+                            width, height);
+            case 4: hq4x_32(static_cast<uint32_t*>(const_cast<void*>(srcData)),
+                            static_cast<uint32_t*>(dstData),
+                            width, height);
+        }
+        return true;
+    }
+    
+    static bool scale2x(const void* srcData, void* dstData, int width, int height, int scaleVal) {
+        scale2x_scale(scaleVal,
+                      dstData, 4 * width * scaleVal,
+                      srcData, 4 * width,
+                      4, width, height);
+        return true;
+    }
+    
+} /* namespace scale */
 
-static bool scale_scale2x(const void* srcData, void* dstData, int width, int height, int scaleVal) {
-    scale2x_scale(scaleVal,
-                  dstData, 4 * width * scaleVal,
-                  srcData, 4 * width,
-                  4, width, height);
-    return true;
-}
-
-static const std::array<Algorithm, 3> algorithms{{
-    { "xbrz",    2, 5,  scale_xbrz    },
-    { "hqx",     2, 4,  scale_hqx     },
-    { "scale2x", 2, 4,  scale_scale2x }
+static const std::array<algorithm, 3> algorithms{{
+    { "xbrz",    2, 5,  scale::xbrz    },
+    { "hqx",     2, 4,  scale::hqx     },
+    { "scale2x", 2, 4,  scale::scale2x }
 }};
 
-Algorithm const* getAlgorithm(const char* name) {
+algorithm const* get_algorithm(const char* name) {
     for (int i = 0; i < algorithms.size(); ++i) {
-        Algorithm const* algorithm = &algorithms[i];
-        if (std::strcmp(name, algorithm->name) == 0) { return algorithm; }
+        algorithm const* algo = &algorithms[i];
+        if (std::strcmp(name, algo->name) == 0) { return algo; }
     }
     return nullptr;
 }
@@ -83,26 +87,26 @@ int depixelate_main(int argc, const char* argv[]) {
             std::printf("Usage: %s <algorithm> <scale> <srcfile> <dstfile>\n", argv[0]);
             std::printf("Algorithms:\n");
             for (int i = 0; i < algorithms.size(); ++i) {
-                Algorithm const* algorithm = &algorithms[i];
-                std::printf("- %s\n", algorithm->name);
+                algorithm const* algo = &algorithms[i];
+                std::printf("- %s\n", algo->name);
             }
             return EXIT_FAILURE;
         }
         
-        const char* algoName = argv[1];
+        const char* algoname = argv[1];
         int         scale    = std::atoi(argv[2]);
         NSString*   srcFile  = [NSString stringWithUTF8String:argv[3]];
         NSString*   dstFile  = [NSString stringWithUTF8String:argv[4]];
         
-        Algorithm const* algorithm = getAlgorithm(algoName);
+        algorithm const* algo = get_algorithm(algoname);
         
-        if (algorithm == nullptr) {
-            std::printf("%s: Unknown algorithm\n", algoName);
+        if (algo == nullptr) {
+            std::printf("%s: Unknown algorithm\n", algoname);
             return EXIT_FAILURE;
         }
         
-        if (scale < algorithm->minScale || scale > algorithm->maxScale) {
-            std::printf("Scale must be from %d to %d\n", algorithm->minScale, algorithm->maxScale);
+        if (scale < algo->minScale || scale > algo->maxScale) {
+            std::printf("Scale must be from %d to %d\n", algo->minScale, algo->maxScale);
             return EXIT_FAILURE;
         }
         
@@ -117,10 +121,10 @@ int depixelate_main(int argc, const char* argv[]) {
         std::size_t dstByteCount = rep.pixelsWide * rep.pixelsHigh * rep.bitsPerPixel * scale;
         void* dstData            = std::malloc(dstByteCount);
         
-        if (!algorithm->scaler(srcData, dstData,
-                               static_cast<int>(rep.pixelsWide),
-                               static_cast<int>(rep.pixelsHigh),
-                               scale)) {
+        if (!algo->scaler(srcData, dstData,
+                          static_cast<int>(rep.pixelsWide),
+                          static_cast<int>(rep.pixelsHigh),
+                          scale)) {
             std::printf("Scaling failed!\n");
             std::free(dstData);
             return EXIT_FAILURE;
