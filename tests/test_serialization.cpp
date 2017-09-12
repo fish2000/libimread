5ac9a26dcbcaf88ba22976e05a6c9ef64a4d04ef
Alexander Böhn <fish2000@gmail.com>
2017-09-11 20:47:29 -0400

    Tests for serialization I/O helpers

--- tests/test_serialization.cpp
+++ tests/test_serialization.cpp
@@ -6,14 +6,20 @@
 #include <libimread/store.hh>
 #include <libimread/serialization.hh>
 
+#include <libimread/ext/filesystem/path.h>
+#include <libimread/ext/filesystem/temporary.h>
+
 #include "include/test_data.hpp"
 #include "include/catch.hpp"
 
-#define SERIALIZATION_PRINT_DUMPS 1
+#define SERIALIZATION_PRINT_DUMPS 0
 
 namespace {
     
     using store::stringmap;
+    using filesystem::path;
+    using filesystem::TemporaryName;
+    using filesystem::NamedTemporaryFile;
     
     TEST_CASE("[serialization] Test store::detail::*_dumps() and store::detail::*_impl()",
               "[serialization-test-store-detail-dumps-store-detail-impl]")
@@ -51,6 +57,79 @@ namespace {
                     stringmap destination;                                                                                                  \
                     std::string dump = store::detail::__name__##_dumps(source.mapping());                                                   \
                     store::detail::__name__##_impl(dump, &destination);                                                                     \
+                    CHECK(source.to_string() == destination.to_string());                                                                   \
+                }
+        
+        #endif
+        
+        DEFINE_SERIALIZATION_FORMAT_SECTION(ini);
+        DEFINE_SERIALIZATION_FORMAT_SECTION(json);
+        DEFINE_SERIALIZATION_FORMAT_SECTION(plist);
+        DEFINE_SERIALIZATION_FORMAT_SECTION(yaml);
+    }
+    
+    #undef DEFINE_SERIALIZATION_FORMAT_SECTION
+    
+    TEST_CASE("[serialization] Test string I/O helper functions",
+              "[serialization-test-string-io-helper-functions]")
+    {
+        stringmap source;
+        
+        source.set("yo",            "dogg");
+        source.set("i_heard",       "you like");
+        source.set("serialization", "and all of its methods");
+        source.set("so",            "we put some");
+        source.set("strings",       "in your strings so you can");
+        source.set("encode",        "while you decode and");
+        source.set("decode",        "while you encode");
+        source.set("etc",           "and soforth");
+        
+        #if(SERIALIZATION_PRINT_DUMPS)
+        std::string asterisks(120, '*');
+        
+            #define DEFINE_SERIALIZATION_FORMAT_SECTION(__name__)                                                                           \
+                                                                                                                                            \
+                SECTION("[serialization] » Testing store::detail::" # __name__ "_dumps() and store::detail::" # __name__ "_impl()")         \
+                {                                                                                                                           \
+                    stringmap destination;                                                                                                  \
+                    TemporaryName tn("." # __name__);                                                                                       \
+                    NamedTemporaryFile tf("." # __name__);                                                                                  \
+                    REQUIRE(tn.pathname != tf.filepath);                                                                                    \
+                    std::string dump = store::detail::__name__##_dumps(source.mapping());                                                   \
+                    CHECK(store::detail::string_dump(dump, tn.pathname.str()));                                                             \
+                    tf.open();                                                                                                              \
+                    tf.stream << dump;                                                                                                      \
+                    tf.close();                                                                                                             \
+                    WTF("SERIALIZATION - " # __name__ " dump:", "\n" + asterisks + "\n" + dump + "\n" + asterisks);                         \
+                    store::detail::__name__##_impl(dump, &destination);                                                                     \
+                    std::string manual_load = store::detail::string_load(tn.pathname.str());                                                \
+                    std::string auto_load = store::detail::string_load(tf.filepath.str());                                                  \
+                    CHECK(manual_load == auto_load);                                                                                        \
+                    CHECK(manual_load == dump);                                                                                             \
+                    CHECK(auto_load == dump);                                                                                               \
+                    CHECK(source.to_string() == destination.to_string());                                                                   \
+                }
+        
+        #else
+            #define DEFINE_SERIALIZATION_FORMAT_SECTION(__name__)                                                                           \
+                                                                                                                                            \
+                SECTION("[serialization] » Testing store::detail::" # __name__ "_dumps() and store::detail::" # __name__ "_impl()")         \
+                {                                                                                                                           \
+                    stringmap destination;                                                                                                  \
+                    TemporaryName tn("." # __name__);                                                                                       \
+                    NamedTemporaryFile tf("." # __name__);                                                                                  \
+                    REQUIRE(tn.pathname != tf.filepath);                                                                                    \
+                    std::string dump = store::detail::__name__##_dumps(source.mapping());                                                   \
+                    CHECK(store::detail::string_dump(dump, tn.pathname.str()));                                                             \
+                    tf.open();                                                                                                              \
+                    tf.stream << dump;                                                                                                      \
+                    tf.close();                                                                                                             \
+                    store::detail::__name__##_impl(dump, &destination);                                                                     \
+                    std::string manual_load = store::detail::string_load(tn.pathname.str());                                                \
+                    std::string auto_load = store::detail::string_load(tf.filepath.str());                                                  \
+                    CHECK(manual_load == auto_load);                                                                                        \
+                    CHECK(manual_load == dump);                                                                                             \
+                    CHECK(auto_load == dump);                                                                                               \
                     CHECK(source.to_string() == destination.to_string());                                                                   \
                 }
         
