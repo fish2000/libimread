14f14289913ef179ec5b07495bd0ed202bbeaa76
Alexander Böhn <fish2000@gmail.com>
2017-11-30 14:07:35 -0500

    Some nagging little build-system updates ... e.g. all include files that aren’t part of the C standard library     or the STL have their presence properly checked in build_config.cmake ... Plus some other assorted, truly small-potatoes tweaks as well

--- cmake/CheckDevEntry.cmake
+++ cmake/CheckDevEntry.cmake
@@ -3,12 +3,12 @@ macro(check_dev_entry dev_entry var)
     message(STATUS "Checking for /dev/${dev_entry}")
     if(EXISTS "/dev/${dev_entry}")
         message(STATUS "Checking for /dev/${dev_entry} - found")
-        set(${var} TRUE
+        set(${var} 1
             CACHE INTERNAL
             "Dev entry ${dev_entry} presence")
     else()
         message(STATUS "Checking for /dev/${dev_entry} - not found")
-        set(${var} FALSE
+        set(${var} 0
             CACHE INTERNAL
             "Dev entry ${dev_entry} presence")
     endif()
--- cmake/build_config.cmake
+++ cmake/build_config.cmake
@@ -33,42 +33,64 @@ include(CheckTypeSize)
 include(CheckFunctionExists)
 include(CheckDevEntry)
 
-# Check required headers
+# Check for architecture-dependent headers:
 check_include_file(fcntl.h                              HAVE_FCNTL_H)
 check_include_file(dirent.h                             HAVE_DIRENT_H)
 check_include_file(unistd.h                             HAVE_UNISTD_H)
 check_include_file(dlfcn.h                              HAVE_DLFCN_H)
 check_include_file(pthread.h                            HAVE_PTHREAD_H)
+check_include_file(utime.h                              HAVE_UTIME_H)
 check_include_file(pwd.h                                HAVE_PWD_H)
 check_include_file(glob.h                               HAVE_GLOB_H)
 check_include_file(wordexp.h                            HAVE_WORDEXP_H)
+
+# Check for (processor-dependant) intrinsics headers:
 check_include_file(immintrin.h                          HAVE_IMMINTRIN_H)
 check_include_file(x86intrin.h                          HAVE_X86INTRIN_H)
-# check_include_file(immintrin.h                          HAVE_INTRINSICS_HEADER)
-# check_include_file(x86intrin.h                          HAVE_INTRINSICS_HEADER)
 
+# Check for (possibly non-standard) C++ headers:
 check_include_file_cxx(cxxabi.h                         HAVE_CXXABI_HH)
 check_include_file_cxx(experimental/string_view         HAVE_EXPERIMENTAL_STRINGVIEW_HH)
 check_include_file_cxx(string_view                      HAVE_STRINGVIEW_HH)
+check_include_file_cxx(cfenv                            HAVE_CFENV_HH)
 
+# Check for operating-system-dependent headers:
 check_include_file(sys/ioctl.h                          HAVE_SYS_IOCTL_H)
+check_include_file(sys/sysctl.h                         HAVE_SYS_SYSCTL_H)
 check_include_file(sys/types.h                          HAVE_SYS_TYPES_H)
 check_include_file(sys/stat.h                           HAVE_SYS_STAT_H)
 check_include_file(sys/time.h                           HAVE_SYS_TIME_H)
-check_include_file(sys/sendfile.h                       HAVE_SYS_SENDFILE_H)
 check_include_file(sys/mman.h                           HAVE_SYS_MMAN_H)
+check_include_file(sys/resource.h                       HAVE_SYS_RESOURCE_H)
+check_include_file(sys/socket.h                         HAVE_SYS_SOCKET_H)
+check_include_file(sys/sendfile.h                       HAVE_SYS_SENDFILE_H)
+check_include_file(sys/uio.h                            HAVE_SYS_UIO_H)
+check_include_file(sys/extattr.h                        HAVE_SYS_EXTATTR_H)
+check_include_file(sys/xattr.h                          HAVE_SYS_XATTR_H)
+check_include_file(netinet/in.h                         HAVE_NETINET_IN_H)
+
+# Check for headers known to be non-standard:
+check_include_file(alloca.h                             HAVE_ALLOCA_H)
+# check_include_file(config.h                             HAVE_CONFIG_H)
+check_include_file(getopt.h                             HAVE_GETOPT_H)
+
 
 if(APPLE)
     
+    # Check for headers specific to Darwin and OS X:
     check_include_file(copyfile.h                       HAVE_COPYFILE_H)
     check_include_file(mach-o/dyld.h                    HAVE_MACHO_DYLD_H)
     check_include_file(objc/message.h                   HAVE_OBJC_MESSAGE_H)
     check_include_file(objc/runtime.h                   HAVE_OBJC_RUNTIME_H)
+    check_include_file(libkern/OSTypes.h                HAVE_LIBKERN_OSTYPES_H)
+    
+    # Check for /dev entries (only on OS X for now):
     check_dev_entry("autofs_nowait"                     HAVE_AUTOFS_NOWAIT)
     check_dev_entry("autofs_notrigger"                  HAVE_AUTOFS_NOTRIGGER)
     
 else(APPLE)
     
+    # Set all results to false for checks related to OS X:
     set(HAVE_COPYFILE_H NO
         CACHE INTERNAL
         "<copyfile.h> presence"
@@ -89,6 +111,11 @@ else(APPLE)
         "<objc/runtime.h> presence"
         FORCE)
     
+    set(HAVE_LIBKERN_OSTYPES_H NO
+        CACHE INTERNAL
+        "<libkern/OSTypes.h> presence"
+        FORCE)
+    
     set(HAVE_AUTOFS_NOWAIT NO
         CACHE INTERNAL
         "/dev/autofs_nowait presence"
@@ -101,6 +128,7 @@ else(APPLE)
     
 endif(APPLE)
 
+# Check for a few (possibly non-standard) POSIX-ish functions:
 set(CMAKE_REQUIRED_INCLUDES limits.h stdlib.h)
 set(CMAKE_REQUIRED_DEFINITIONS -D_GNU_SOURCE)
 check_function_exists(canonicalize_file_name            HAVE_CANONICALIZE_FILE_NAME_F)
@@ -108,6 +136,7 @@ check_function_exists(realpath                          HAVE_REALPATH_F)
 set(CMAKE_REQUIRED_INCLUDES)
 set(CMAKE_REQUIRED_DEFINITIONS)
 
+# Figure out the deal with 128-bit integer types:
 set(CMAKE_EXTRA_INCLUDE_FILES stdint.h)
 check_type_size("__int128_t"                            INT128_T)
 check_type_size("__uint128_t"                           UINT128_T)
